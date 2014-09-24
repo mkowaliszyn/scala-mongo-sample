1751d82e98f602079c11a8fe7575d5e383b523e9
Michal Kowaliszyn <mkowaliszyn@gilt.com>
2014-09-24 17:17:25 +0100

    morhpia added

--- Sample.scala
+++ Sample.scala
@@ -1,4 +1,13 @@
+import com.mongodb.{Mongo, WriteConcern}
 import com.mongodb.casbah.Imports._
+
+import com.github.jmkgreen.morphia.Morphia
+import com.github.jmkgreen.morphia.annotations._
+
+import org.bson.types.ObjectId
+import java.util.{List => JList, ArrayList => JArrayList}
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters.seqAsJavaListConverter
 
 /*
 	More: 
@@ -6,18 +15,61 @@ import com.mongodb.casbah.Imports._
 		http://www.scala-sbt.org/0.13/tutorial/Running.html
 */
 
-object ScalaMongoSampleApp {
+
+@Entity("temp_people_morhpia")	
+class Person {  
+
+	@Id
+  var id: ObjectId = _
+
+	var name: String = _
+	
+	var age: Int = _
+
+	var favNums: JList[Int] = _
+
+	override def toString = s"id=$id, name=$name, age=$age, favNums=$favNums"
+}
+
+object MongoWithMorphiaApp {
   def main(args: Array[String]): Unit = {
+		// usingMongo()
+		usingMorphia()
+	}
 
-    val mongoConn = MongoConnection()
-		val sampleColl = mongoConn("test")("temp-people")
+	def usingMongo(): Unit = {
+		val mongoConn = MongoConnection()
+		val sampleColl = mongoConn("test")("temp_people")
 		sampleColl.drop()
-
 		sampleColl.insert(MongoDBObject("name" -> "Michal", "age" -> 128))
 		sampleColl.insert(MongoDBObject("name" -> "Rafal"))
-		
 		val samplePerson = sampleColl.findOne("age" $exists true)
-
 		println(samplePerson)
+	}
+
+  def usingMorphia(): Unit = {
+  	val mongo = new Mongo()
+
+		val mongoDs = new Morphia().createDatastore(mongo, "test")
+
+		val p1 = new Person
+		p1.name = "name-1"
+		p1.age = 128
+		p1.favNums = List(1, 2, 4).asJava
+
+		val p2 = new Person
+		p2.name = "name-2"
+		p2.age = 256
+		p2.favNums = List(8, 16, 32).asJava
+
+		mongoDs.save(p1, WriteConcern.SAFE)
+		mongoDs.save(p2, WriteConcern.SAFE)
+
+		val people = mongoDs.createQuery(classOf[Person]).asList
+
+		for (person <- people) {
+			println(person)
+		}
+		
   }
 }
--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.1-SNAPSHOT"
 scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
-  "org.mongodb" %% "casbah" % "2.7.3"
+  "org.mongodb" %% "casbah" % "2.7.3",
+  "com.github.jmkgreen.morphia" % "morphia" % "1.2.3"
 )
